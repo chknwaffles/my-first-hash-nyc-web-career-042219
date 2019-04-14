8d8217fb5d64aff0d7479bc1c62998456390a3fc
Won Kim <wonsae.kim@gmail.com>
2019-04-14 19:51:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,7 +9,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  
 
 end
 
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest
+  shipping_manifest["oil paintings"]
 end
 
 def adding
